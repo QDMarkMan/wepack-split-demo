eda15a0901f22d3459162677322dd62d169f5901
etongfu <531202571@qq.com>
2020-07-21 22:09:14 +0800

    update: base file

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class About extends React.Component {
+  
+  render() {
+    return (<div>
+      11111
+    </div>)
+  }
+}
+
+ReactDom.render(<About />, document.querySelector('#app'))
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+module.exports = {
+    mode:"development",
+    entry: path.resolve(__dirname, './src/entry.js'),
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js',
+      publicPath:'dist/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    devServer:{
+      contentBase: path.join(__dirname),
+      compress: true,
+      inline: true,
+      host: '127.0.0.1',
+      port: 8001,
+      open: false
+    }
+}
